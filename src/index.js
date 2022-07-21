ae13701e0acba3581483b153a7ddd3e1f72f3b5b
Matheus Vitor <vitor.lp45@gmail.com>
2022-07-21 17:22:47 -0300

    organizando

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const routes = require("./routes");
 require('dotenv').config();
 
 const app = express();
 
-mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
-//TODO: Error handling on crash connect mongoose
+
 
 app.use(cors());
 app.use(express.json());
